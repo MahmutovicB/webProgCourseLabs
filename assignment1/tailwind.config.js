6bb44441140d43e7995b5d02aa55fdfeceeebf8e
Benjamin Mahmutovic <benjamin.mahmutovic@code-it.ba>
2024-11-17 19:17:34 +0100

    Added products, about us and login page

---
+++ assignment1/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/*.html', // Add your valid paths here
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
